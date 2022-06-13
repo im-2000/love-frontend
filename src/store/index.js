ac259429b835aa9c9821d123c42d48efb0f96204
elkatrachuk <elkatrachuk@gmail.com>
2022-06-13 23:21:45 +0200

    add filters

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import appStateReducer from "./appState/slice";
 import userReducer from "./user/slice";
+import peopleSlice from "./people/slice";
 
 export default configureStore({
   reducer: {
     appState: appStateReducer,
     user: userReducer,
+    people: peopleSlice,
   },
 });
